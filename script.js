f62f99f3d6deab4b1d6a73a7f13f57d451824be1
Garrett Hufstedler <garretthufstedler@gmail.com>
2022-03-17 15:39:50 -0400

    Added function to display numbers on click and clear display on click

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 //add function
-const add = function (x, y) {
+const add = function add (x, y) {
     return x + y;
-}
+};
 console.log(add(2,5));
 //subtract 
-const subtract = function (x, y) {
+const subtract = function subtract (x, y) {
     return x - y;
-}
+};
 console.log(subtract(2,5));
 //multiply
-const multiply = function (x, y) {
+const multiply = function multiply (x, y) {
     return x * y;
-}
+};
 console.log(multiply(2,5));
 //divide 
-const divide = function (x, y) {
+const divide = function divide (x, y) {
     return x / y;
-}
+};
 console.log(divide(2,5));
 
 //operator function for calculator
@@ -24,7 +24,7 @@ function operate (operator, x, y) {
     if (operator === "+" || "add") {
         return x + y;
     } else if (operator === "-") {
-        return x -y ;
+        return x - y ;
     } else if (operator === "*") {
         return x * y;
     } else if (operator ==="/") {
@@ -32,3 +32,19 @@ function operate (operator, x, y) {
     }
 };
 console.log(operate("+",3,1));
+
+//displays numbers from buttons in display
+const numbers = document.querySelectorAll('button.number');
+const display = document.getElementById('display');
+for (i=0; i < numbers.length; i++) {
+    numbers[i].addEventListener('click', function(event) {
+        display.innerText = display.innerText + event.currentTarget.value;
+    })
+};
+//clears display with CE (Clear Entry) button click
+const clearEntry = document.getElementById('clear');
+clearEntry.addEventListener('click', function(event) {
+    display.innerText = '';
+});
+
+//
