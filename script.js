0e6c6385ffd75b0e0a6cd41bd56d958155c8bf97
Garrett Hufstedler <garretthufstedler@gmail.com>
2022-03-20 14:39:11 -0400

    Modified operator function to add first two numbers. Works with first calculation, needs to work with operator applied to result of first calculation

--- script.js
+++ script.js
@@ -1,50 +1,140 @@
+const display = document.getElementById('display');
+const operators = document.querySelectorAll("#plusMinus, #percentage, #addition, #subtraction, #multiplication, #division, #equals");
+let firstNum = '';
+let secondNum = '';
+let operator = '';
+let result = operate(operator);
+
 //add function
-const add = function add (x, y) {
-    return x + y;
+const add = function add (firstNum, secondNum) {
+    return (parseInt(firstNum) + parseInt(secondNum));
 };
 console.log(add(2,5));
+
 //subtract 
-const subtract = function subtract (x, y) {
-    return x - y;
+const subtract = function subtract (firstNum, secondNum) {
+    return (parseInt(firstNum) - parseInt(secondNum));
 };
 console.log(subtract(2,5));
+
 //multiply
-const multiply = function multiply (x, y) {
-    return x * y;
+const multiply = function multiply (firstNum, secondNum) {
+    return (parseInt(firstNum) * parseInt(secondNum));
 };
 console.log(multiply(2,5));
+
 //divide 
-const divide = function divide (x, y) {
-    return x / y;
+const divide = function divide (firstNum, secondNum) {
+    return (parseInt(firstNum) / parseInt(secondNum));
 };
 console.log(divide(2,5));
 
 //operator function for calculator
-function operate (operator, x, y) {
-    if (operator === "+" || "add") {
-        return x + y;
-    } else if (operator === "-") {
-        return x - y ;
-    } else if (operator === "*") {
-        return x * y;
-    } else if (operator ==="/") {
-        return x / y;
+function operate (operator, firstNum, secondNum) {
+    if (operator == "+") {
+        add(firstNum, secondNum);
+        return add(firstNum, secondNum);
+    } else if (operator == "-") {
+        subtract(firstNum, secondNum);
+    } else if (operator == "*") {
+        multiply(firstNum, secondNum);
+    } else if (operator == "/") {
+        divide(firstNum, secondNum);
     }
+    
 };
-console.log(operate("+",3,1));
+console.log(operate("+"));
 
-//displays numbers from buttons in display
-const numbers = document.querySelectorAll('button.number');
-const display = document.getElementById('display');
-for (i=0; i < numbers.length; i++) {
-    numbers[i].addEventListener('click', function(event) {
-        display.innerText = display.innerText + event.currentTarget.value;
+//applies click listeners to all buttons and runs calc(e) function on click
+const buttons = document.querySelectorAll('button');
+for (i=0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function(e) {
+        if (e.target.matches("button")){
+            calc(e);
+        } else if (e.target.matches("equals")) {
+            operate(operator);
+        }
     })
 };
-//clears display with CE (Clear Entry) button click
+console.log();
+
+// clears display with CE (Clear Entry) button click
 const clearEntry = document.getElementById('clear');
 clearEntry.addEventListener('click', function(event) {
     display.innerText = '';
 });
 
+// work in progress below
+//working calc function
+// const addition = document.getElementById('addition');
+// const subtraction = document.getElementById('subtraction');
+// const multiplication = document.getElementById('multiplication');
+// const division = document.getElementById('division');
+// const equals = document.getElementById('equals');
+// const percentage = document.getElementById('percentage');
+// const plusMinus = document.getElementById('plusMinus');
+
+
+
+function calc(e) {
+    if (e.target.className === "number") {
+        if (operator === '') {
+            firstNum += e.target.innerText; 
+            display.innerText += e.target.innerText;
+        } else {
+            display.innerText = '';
+            secondNum += e.target.value;
+            display.innerText += e.target.innerText;
+        }
+        // display.innerText += e.currentTarget.value;
+    }
+}
+
+ops = Array.prototype.slice.call(operators,0);
+
+ops.forEach(op => {
+    op.addEventListener("click", e => {
+        if (e.target.innerText !== "=") {
+            operator = e.target.innerText;
+            console.log(firstNum);
+            console.log(operator);
+        } else {
+            console.log(secondNum);
+            result = operate(operator, firstNum, secondNum);
+            display.innerText = '';
+            display.innerText += result;
+            console.log(operate(operator, firstNum, secondNum));
+             
+            // display.innerText = secondNum;
+        };
+    });
+});
+
+
+// function calc(e) {
+//     const key = e.target; 
+//     const action = key.dataset.action; 
+
+//     if (action === "addition" || 
+//         action === "subtraction"|| 
+//         action === "multiplication" ||
+//         action === "division"
+//     ) {
+//         console.log("operator key");
+//     }
+// }
+
+//my own non functioning addition on 
+// addition.addEventListener('click', function(event) {
+//     x = display.innerText;
+//     y = button
+//     return operate("+",x,y);     
+// });
+
+// adds event listener to all buttons
+// const keys = document.querySelector("#calcContent");
+// keys.addEventListener("click", (e) => {
+//     if (e.target.matches("button")){
+//         calc(e);
+//     }
 //
